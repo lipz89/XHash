48dfe9968ea0254c7eb39153ddf11ef139ea2693
23955618 <23955618@qq.com>
2018-10-09 16:03:27 +0800

    修改python版本，增加验证函数

--- XHash.py
+++ XHash.py
@@ -9,17 +9,46 @@ from base64 import b64decode
 
 import sys
 
-def createHash(input):
+def create(input):
     saltBytes=b("").join([pack("@H", randint(0, 0xffff)) for i in range(12)])
+    rawhash = PBKDF2(input, saltBytes, 5000).read(24)
+    hash=b64encode(rawhash).decode('us-ascii')	
     saltB64=b64encode(saltBytes)
     salt=saltB64.decode('us-ascii')
-    rawhash = PBKDF2(input, b64decode(salt), 5000).read(24)
-    hash=b64encode(rawhash).decode('us-ascii')
     result=salt+hash
     return result
- 
+
+def validate(input,correctHash):
+	salt=correctHash[:32]
+	hash=correctHash[32:]
+	rawhash = PBKDF2(input, b64decode(salt), 5000).read(24)
+	hash1=b64encode(rawhash).decode('us-ascii')
+	return slowEquals(hash,hash1)
+
+def slowEquals(a, b):
+	ba=b64decode(a)
+	bb=b64decode(b)
+	diff = len(ba) ^ len(bb);
+	for i in range(len(ba)):
+		diff=diff|(ba[i]^bb[i])
+	return diff == 0
 
 if __name__=='__main__':
     input="123456"
-    hash=createHash(input)
-    print(hash)
+    print(input)	
+    hash=create(input)
+    print(hash)	
+    valid=validate(input,hash)
+    print(valid)
+    input2="1234560"
+    print(input2)	
+    hash2=create(input2)
+    print(hash2)	
+    valid=validate(input2,hash2)
+    print(valid)
+    print("print false:")
+    valid=validate(input,hash2)
+    print(valid)
+    valid=validate(input2,hash)
+    print(valid)
+
